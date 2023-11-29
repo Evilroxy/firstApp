08e32fe9b72021be375596f0a7d1d19adf460fc6
Evilroxy <cvanelstraete@edenschool.fr>
2023-11-29 09:20:42 +0100

    exercise w/ olivier

--- main.js
+++ main.js
@@ -1,24 +1,14 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const inputHTML = document.getElementById("inputHTML");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+function AgePopup() {
+  let valeurage = inputHTML.value;
+ 
+  if (valeurage 6+7) {
+    alert("Poussin !");
+  } else if (valeurage 8+9) {
+    alert("Pupille !");
+  } else if (valeurage 10+11 ) {
+    alert("Minime !");
+  } else if (valeurage <= 12 ) {
+    alert("Cadet !");
+  }
